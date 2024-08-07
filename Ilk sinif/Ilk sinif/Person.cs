4de735ea94ac86f89ce0712a41e92fc3a1452f81
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-07 13:51:46 +0300

    Add files via upload

---
+++ Ilk sinif/Ilk sinif/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ilk_sinif
+{
+    public class Person
+    {
+        public string ad;
+        public string soyad;
+        public string dogumtarihi;
+
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        public string Soyad
+        {
+            get { return soyad;}
+            set { soyad = value; }
+        }
+        public string Dogumtarihi
+        {
+            get { return dogumtarihi; }
+            set { dogumtarihi = value; }
+        }
+        public void OgrenciBilgi()
+        {
+            Console.WriteLine($"Ogrenci adı={ad}, Soyad = {soyad}, Dogum tarihi = {dogumtarihi}");
+        }
+        public void OgretmenBilgi()
+        {
+            Console.WriteLine($"Ogretmen adı={ad}, Soyad = {soyad}, Dogum tarihi = {dogumtarihi}");
+        }
+    }
+}
---
+++ Ilk sinif/Ilk sinif/Program.cs
@@ -0,0 +1,18 @@
+﻿using Ilk_sinif;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person ogrenci = new Person();
+        ogrenci.ad = "Mustafa";
+        ogrenci.soyad = "Çınar";
+        ogrenci.dogumtarihi = "11.06.1999";
+        Person ogretmen = new Person();
+        ogretmen.ad = "Eren";
+        ogretmen.soyad = "Bozkus";
+        ogretmen.dogumtarihi = "12.01.1996";
+        ogrenci.OgrenciBilgi();
+        ogretmen.OgretmenBilgi();
+    }
+}
