d65605d8bdc3c5d649f8713aa9c0ea96b2bf2803
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-08 14:56:36 +0300

    Add files via upload

---
+++ Constructor/Constructor/Bebek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Bebek
+    {
+        public DateTime DogumTarihi;
+        public string _Ad;
+        public string _Soyad;
+        public Bebek()
+        {
+            Console.WriteLine("Ingaaa");
+            DogumTarihi = DateTime.Now;
+            Console.WriteLine($"Bebeğin doğum tarihi:{DogumTarihi}");
+        }
+        public Bebek(string Ad, string Soyad)
+        {
+            _Ad = Ad;
+            _Soyad = Soyad;
+            DogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaa");
+            Console.WriteLine($"Bebeğin adı={_Ad}, Soyadı={_Soyad}, Doğum tarihi:{DogumTarihi}");
+        }
+
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,14 @@
+﻿using Constructor;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebek bebek1 = new Bebek("Mustafa", "Çınar");
+            Bebek bebek2 = new Bebek();
+
+        }
+    }
+}
