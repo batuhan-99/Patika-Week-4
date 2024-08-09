8f41efb7a7bc0ecc6eb47cf18c0f6dc36518a303
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-09 11:13:40 +0300

    Add files via upload

---
+++ Patika Kutuphane/Patika Kutuphane/Kitap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patika_Kutuphane
+{
+    public class Kitap //Kitap adlı bir sınıf tanımlandı.Bu sınıf ile kitap ile ilgili nesnelerin özelliklerini ve davranışları tanımlandı
+    {
+        // Aşağıdaki Property'ler ile sınıfın özelliklerini tanımladım
+        public string _ad;
+        public string _yazaradi;
+        public string _yazarsoyadi;
+        public int _sayfasayisi;
+        public string _yayinevi;
+        public DateTime kayittarihi;
+        
+        //Aşğıda 2 tane constructor tanımlandı. Bu constructorlar ile nesneye başlangıç değerlerini verebildik.
+        public Kitap()
+        {
+            kayittarihi = DateTime.Now;
+            Console.WriteLine($"Kayıt tarihi:{kayittarihi}");
+        } 
+        public Kitap(string ad, string yazaradi, string yazarsoyadi, int sayfasayisi, string yayinevi)
+        {
+            _ad = ad;
+            _yazaradi = yazaradi;
+            _yazarsoyadi = yazarsoyadi;
+            _sayfasayisi = sayfasayisi;
+            _yayinevi = yayinevi;
+            kayittarihi = DateTime.Now;
+            Console.WriteLine($"Kitap adı={_ad}, Yazar adı={_yazaradi}, Yazar soyadı={_yazarsoyadi}, Sayfa sayısı={_sayfasayisi}, Yayınevi={_yayinevi}");
+            Console.WriteLine($"Kayıt tarihi:{kayittarihi}");
+        }
+    }
+}
---
+++ Patika Kutuphane/Patika Kutuphane/Program.cs
@@ -0,0 +1,24 @@
+﻿using Patika_Kutuphane;
+
+namespace Patika_Kutuphane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new anahtar kelimesi ile kitap sınıfından yeni bir nesne oluşturdum
+            Kitap kitap1 = new Kitap("Bir Kitap", "Ahmet", "Yılmaz", 320, "Alfa Yayınları");
+            Kitap kitap2 = new Kitap("Başka Bir Kitap", "Ayşe", "Karan", 215, "Beta Yayıncılık");
+            Kitap kitap3 = new Kitap("Daha Başka Kitap", "Mehmet", "Demir", 450, "Gamma Yayınları");
+            Kitap kitap4 = new Kitap("Son Kitap", "Elif", "Şahin", 275, "Delta Yayıncılık");
+            Kitap kitap5 = new Kitap("Örnek Kitap", "Selin", "Yurt", 198, "Epsilon Yayınları");
+
+            Kitap kitap6 = new Kitap();
+            Kitap kitap7 = new Kitap();
+            Kitap kitap8 = new Kitap();
+            Kitap kitap9 = new Kitap();
+            Kitap kitap10 = new Kitap();
+
+        }
+    }
+}
