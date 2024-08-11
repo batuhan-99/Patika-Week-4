75c7bc268666f2cc649cf3c3ecd3ba19d8416e44
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-11 12:10:33 +0300

    Add files via upload

---
+++ Inheritance/Inheritance/BaseKisi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public abstract class BaseKisi
+    {
+        public string _ad { get; set; }
+        public string _soyad { get; set; }
+
+        public virtual void Bilgi()
+        {
+            Console.WriteLine($"İsim : {_ad}, Soyisim : {_soyad}");
+        }
+
+    }
+    public class Ogrenci : BaseKisi
+    {
+        public int No { get; set; }
+        public  override void Bilgi()
+        {
+            Console.WriteLine($"Ogrenci Numarası : {No}");
+            base.Bilgi();
+        }
+    }
+    public class Ogretmen : BaseKisi
+    {
+        public double maaş {  get; set; }
+        public  override void Bilgi()
+        {
+            Console.WriteLine($"Maaş : {maaş}");
+            base.Bilgi();
+        }
+    } 
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using Inheritance;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            Ogretmen ogretmen = new Ogretmen();
+
+            ogrenci._ad = "Mustafa";
+            ogrenci._soyad = "Çınar";
+            ogrenci.No = 47896;
+            ogretmen._ad = "Eren";
+            ogretmen._soyad = "Bozkurt";
+            ogretmen.maaş = 13500;
+            ogrenci.Bilgi();
+            ogretmen.Bilgi();
+        }
+    }
+}
