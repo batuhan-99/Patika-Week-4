d85f8be35a2a5c0ff18b2d140a7d4c9f8bda874a
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-11 19:23:01 +0300

    Add files via upload

---
+++ Polymorphism/Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+   public class BaseGeometrikSekil
+    {
+        public int genislik {  get; set; }
+        public int yukseklik { get; set; }
+
+        public BaseGeometrikSekil(int Genislik, int Yukseklik)
+        {
+            genislik = Genislik;
+            yukseklik = Yukseklik;
+        }
+        public virtual int AlanHesapla()
+        {
+            return genislik * yukseklik;
+        }
+
+    }
+    public class Kare : BaseGeometrikSekil
+    {
+        public Kare(int genislik, int yukseklik) : base(genislik, yukseklik)
+        {
+        }
+        public override int AlanHesapla()
+        {
+             return base.AlanHesapla();
+        }
+    }
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public Dikdortgen(int genislik, int yukseklik) : base(genislik, yukseklik)
+        {
+        }
+        public override int AlanHesapla()
+        {
+            return base.AlanHesapla();
+        }
+    }
+    public class DikUcgen : BaseGeometrikSekil
+    {
+        public DikUcgen(int genislik, int yukseklik) : base(genislik, yukseklik)
+        {
+        }
+        public override int AlanHesapla()
+        {
+            return base.AlanHesapla()/2;
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using Polymorphism;
+using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kare kare = new Kare(5,6);
+            Dikdortgen dikdortgen = new Dikdortgen( 9,9);
+            DikUcgen dikucgen = new DikUcgen(8, 4);
+            
+            Console.WriteLine($"Kare alanı: {kare.AlanHesapla()}");
+            Console.WriteLine($"Dikdörtgen alanı: {dikdortgen.AlanHesapla()}");
+            Console.WriteLine($"Dik Üçgen alanı: {dikucgen.AlanHesapla()}");
+        }
+    }
+}
