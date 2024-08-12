5467c9a752746a4eadd9d146892fe9779c4acced
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-12 08:30:55 +0300

    Add files via upload

---
+++ Abstraction/Abstraction/Calisan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+        public abstract void Gorev();
+    }
+    public class YazilimGelistirici : Calisan
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Yazılım Geliştiricisi, Yazılım geliştiriyorum.");
+        }
+    }
+    public class ProjeYoneticisi : Calisan
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Proje Yöneticisi, Proje yöneticisi olarak çalışıyorum.");
+        }
+    }
+    public class SatisTemsilcisi : Calisan
+    {
+        
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Satış Temsilcisi, Müşteri ilişkilerini yönetiyorum.");
+        }
+    }
+}
---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using Abstraction;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProjeYoneticisi hasan = new ProjeYoneticisi();
+            hasan.Ad = "Hasan";
+            hasan.Soyad = "Cildirmis";
+            hasan.Departman = "Proje Yönetimi";
+
+            hasan.Gorev();
+
+            YazilimGelistirici ali = new YazilimGelistirici();
+            ali.Ad = "Ali";
+            ali.Soyad = "Yildirim";
+            ali.Departman = "Yazılım Geliştirici";
+
+            ali.Gorev();
+
+            SatisTemsilcisi ayse = new SatisTemsilcisi();
+            ayse.Ad = "Ayşe";
+            ayse.Soyad = "Kaya";
+            ayse.Departman = "Satis Temsilcisi";
+
+            ayse.Gorev();
+
+
+
+        }
+    }
+}
